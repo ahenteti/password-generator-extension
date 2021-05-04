ddb1c3fb9e91c13d8246260e9f81b0eddc7a42f7
ahenteti <ahmad.henteti@gmail.com>
2021-05-04 04:55:45 +0200

    improve generatePassword impl

--- password-generator-chrome-extension/popup.js
+++ password-generator-chrome-extension/popup.js
@@ -1,23 +1,27 @@
-const LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-const NUMBERS = '0123456789';
-const SYMBOLS = '@#$%&*()-+=^';
-const LETTERS_LENGTH = 8;
-const NUMBERS_LENGTH = 4;
-const SYMBOLS_LENGTH = 4;
+var LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+var NUMBERS = '0123456789';
+var SYMBOLS = '@#$%&*()-+=^';
 
-const generatedPassword = document.getElementById('generatedPassword');
-const passwordGeneratorButton = document.getElementById('passwordGeneratorButton');
+var generatedPassword = document.getElementById('generatedPassword');
+var passwordGeneratorButton = document.getElementById('passwordGeneratorButton');
+var lettersLengthInput = document.getElementById('lettersLengthInput');
+var numbersLengthInput = document.getElementById('numbersLengthInput');
+var symbolsLengthInput = document.getElementById('symbolsLengthInput');
+
 passwordGeneratorButton.addEventListener('click', generatePassword);
+lettersLengthInput.addEventListener('change', generatePassword);
+numbersLengthInput.addEventListener('change', generatePassword);
+symbolsLengthInput.addEventListener('change', generatePassword);
 
 function generatePassword() {
-  let newPassword = '';
-  for (i = 0; i < LETTERS_LENGTH; i++) {
+  var newPassword = '';
+  for (i = 0; i < lettersLengthInput.value; i++) {
     newPassword += LETTERS.charAt(Math.floor(Math.random() * LETTERS.length));
   }
-  for (i = 0; i < NUMBERS_LENGTH; i++) {
+  for (i = 0; i < numbersLengthInput.value; i++) {
     newPassword += NUMBERS.charAt(Math.floor(Math.random() * NUMBERS.length));
   }
-  for (i = 0; i < SYMBOLS_LENGTH; i++) {
+  for (i = 0; i < symbolsLengthInput.value; i++) {
     newPassword += SYMBOLS.charAt(Math.floor(Math.random() * SYMBOLS.length));
   }
   newPassword = newPassword.split('');
