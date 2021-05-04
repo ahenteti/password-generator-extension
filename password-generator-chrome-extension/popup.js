98936f771f171b48899a20bbb603af4bb18ccca3
ahenteti <ahmad.henteti@gmail.com>
2021-05-04 12:51:07 +0200

    add password settings to local storage

--- password-generator-chrome-extension/popup.js
+++ password-generator-chrome-extension/popup.js
@@ -1,6 +1,9 @@
 var LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 var NUMBERS = '0123456789';
 var SYMBOLS = '@#$%&*()-+=^';
+var LETTERS_LENGTH_INITIAL_VALUE = 8;
+var NUMBERS_LENGTH_INITIAL_VALUE = 4;
+var SYMBOLS_LENGTH_INITIAL_VALUE = 4;
 
 var generatedPassword = document.getElementById('generatedPassword');
 var passwordGeneratorButton = document.getElementById('passwordGeneratorButton');
@@ -8,11 +11,19 @@ var lettersLengthInput = document.getElementById('lettersLengthInput');
 var numbersLengthInput = document.getElementById('numbersLengthInput');
 var symbolsLengthInput = document.getElementById('symbolsLengthInput');
 
+initLettersLengthInputValue();
+initNumbersLengthInputValue();
+initSymbolsLengthInputValue();
 passwordGeneratorButton.addEventListener('click', generatePassword);
 lettersLengthInput.addEventListener('change', generatePassword);
+lettersLengthInput.addEventListener('change', updateLettersLengthLocalStorageValue);
+
 numbersLengthInput.addEventListener('change', generatePassword);
+numbersLengthInput.addEventListener('change', updateNumbersLengthLocalStorageValue);
+
 symbolsLengthInput.addEventListener('change', generatePassword);
-generatePassword();
+symbolsLengthInput.addEventListener('change', updateSymbolsLengthLocalStorageValue);
+setTimeout(generatePassword, 0);
 
 function generatePassword() {
   // code inspiration: https://stackoverflow.com/questions/9719570/generate-random-password-string-with-requirements-in-javascript
@@ -46,4 +57,55 @@ function copyPassword() {
   tmpTextArea.select();
   document.execCommand('Copy');
   tmpTextArea.remove();
+}
+
+function initLettersLengthInputValue() {
+  chrome.storage.local.get(['LETTERS_LENGTH_LOCAL_STORAGE_KEY'], function (result) {
+    if (result) {
+      lettersLengthInput.value = result.LETTERS_LENGTH_LOCAL_STORAGE_KEY;
+    } else {
+      lettersLengthInput.value = LETTERS_LENGTH_INITIAL_VALUE;
+      updateLettersLengthLocalStorageValue();
+    }
+  });
+}
+
+function updateLettersLengthLocalStorageValue() {
+  chrome.storage.local.set({ LETTERS_LENGTH_LOCAL_STORAGE_KEY: lettersLengthInput.value }, function () {
+    console.log('Value is set to ' + lettersLengthInput.value);
+  });
+}
+
+function initNumbersLengthInputValue() {
+  chrome.storage.local.get(['NUMBERS_LENGTH_LOCAL_STORAGE_KEY'], function (result) {
+    if (result) {
+      numbersLengthInput.value = result.NUMBERS_LENGTH_LOCAL_STORAGE_KEY;
+    } else {
+      numbersLengthInput.value = NUMBERS_LENGTH_INITIAL_VALUE;
+      updateNumbersLengthLocalStorageValue();
+    }
+  });
+}
+
+function updateNumbersLengthLocalStorageValue() {
+  chrome.storage.local.set({ NUMBERS_LENGTH_LOCAL_STORAGE_KEY: numbersLengthInput.value }, function () {
+    console.log('Value is set to ' + numbersLengthInput.value);
+  });
+}
+
+function initSymbolsLengthInputValue() {
+  chrome.storage.local.get(['SYMBOLS_LENGTH_LOCAL_STORAGE_KEY'], function (result) {
+    if (result) {
+      symbolsLengthInput.value = result.SYMBOLS_LENGTH_LOCAL_STORAGE_KEY;
+    } else {
+      symbolsLengthInput.value = SYMBOLS_LENGTH_INITIAL_VALUE;
+      updateSymbolsLengthLocalStorageValue();
+    }
+  });
+}
+
+function updateSymbolsLengthLocalStorageValue() {
+  chrome.storage.local.set({ SYMBOLS_LENGTH_LOCAL_STORAGE_KEY: symbolsLengthInput.value }, function () {
+    console.log('Value is set to ' + symbolsLengthInput.value);
+  });
 }
